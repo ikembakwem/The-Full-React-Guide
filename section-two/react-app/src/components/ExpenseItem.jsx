76e0d3476669d08511ae4fd85f240708434ef67c
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2022-11-05 17:39:15 +0100

    Passed data to component through props

--- section-two/react-app/src/components/ExpenseItem.jsx
+++ section-two/react-app/src/components/ExpenseItem.jsx
@@ -1,16 +1,16 @@
 import './ExpenseItem.css';
 
-export const ExpenseItem = () => {
+export const ExpenseItem = ({ date, title, price }) => {
   const expenseDate = new Date(2022, 10, 5);
   const expenseTitle = 'Health Insurance';
   const expensePrice = 300.99;
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toLocaleDateString()}</div>
+      <div>{date.toLocaleDateString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expensePrice}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">{price}</div>
       </div>
     </div>
   );
