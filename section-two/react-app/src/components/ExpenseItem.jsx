ab5a168eb05fd2f7cc6890a4664315ac01f0aa73
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2022-11-05 19:49:09 +0100

    Included currency in ExpenseItem, Created ExpensesList component to render all ExpenseItem. Created Card component to wrap ExpenseList

--- section-two/react-app/src/components/ExpenseItem.jsx
+++ section-two/react-app/src/components/ExpenseItem.jsx
@@ -8,7 +8,7 @@ export const ExpenseItem = ({ date, title, amount }) => {
 
       <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className="expense-item__price">{amount}</div>
+        <div className="expense-item__price">£{amount}</div>
       </div>
     </div>
   );
---
+++ section-two/react-app/src/components/ExpensesList.jsx
@@ -0,0 +1,50 @@
+import Card from './Card';
+import { ExpenseItem } from './ExpenseItem';
+import './ExpensesList.css';
+
+export const ExpensesList = () => {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+    {
+      id: 'e5',
+      title: 'Health Insurance',
+      amount: 89.99,
+      date: new Date(2022, 10, 5),
+    },
+  ];
+
+  return (
+    <>
+      <Card className="expenses">
+        {expenses.map((expense) => (
+          <ExpenseItem
+            key={expense.key}
+            date={expense.date}
+            amount={expense.amount}
+            title={expense.title}
+          />
+        ))}
+      </Card>
+    </>
+  );
+};
+
+export default ExpensesList;
