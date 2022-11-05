09aad1b49025569ad68339e2e4307fa457888ef8
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2022-11-05 17:29:30 +0100

    Modified ExpenseItem component to display dynamic data

--- section-two/react-app/src/components/ExpenseItem.jsx
+++ section-two/react-app/src/components/ExpenseItem.jsx
@@ -1,11 +1,17 @@
 import './ExpenseItem.css';
 
-export const ExpenseItem = () => (
-  <div className="expense-item">
-    <div>November 5th 2022</div>
-    <div className="expense-item__description">
-      <h2>Health Insurance</h2>
-      <div className="expense-item__price">£300.99</div>
+export const ExpenseItem = () => {
+  const expenseDate = new Date(2022, 10, 5);
+  const expenseTitle = 'Health Insurance';
+  const expensePrice = 300.99;
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toLocaleDateString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expensePrice}</div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
