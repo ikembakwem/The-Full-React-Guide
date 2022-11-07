d3ccdfb3ae33880e8a0499f9a34f4cebc03e19e8
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2022-11-07 21:31:40 +0100

    Updated expense title using React hooks, refactored Card component

--- section-two/expenses-react-app/src/components/ExpensesApp/ExpenseItem.jsx
+++ section-two/expenses-react-app/src/components/ExpensesApp/ExpenseItem.jsx
@@ -1,15 +1,24 @@
+import { useState } from 'react';
+import Card from '../UI/Card';
 import { ExpenseDate } from './ExpenseDate';
 import './ExpenseItem.css';
 
 export const ExpenseItem = ({ date, title, amount }) => {
+  const [expenseTitle, setTitle] = useState(title);
+
+  const clickHandler = () => {
+    setTitle('Updated!');
+  };
+
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={date} />
 
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{expenseTitle}</h2>
         <div className="expense-item__price">£{amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
       </div>
-    </div>
+    </Card>
   );
 };
